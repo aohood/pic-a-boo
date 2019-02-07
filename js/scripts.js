b1c748986580d222379a8b45a77016accb00320d
Aya Abu <someone990@yahoo.com>
2019-02-06 16:28:38 -0800

    add new functions as slide up and down , fade in and out, toggle

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,13 @@ $(function(){
     $("#initially-showing").toggle();
   });
 
+  $(".clickable").click(function(){
+    // $("#panel").fadeIn();
+    // $("#panel").fadeOut();
+    //$("#panel").fadeToggle();
+    $("#panel").slideToggle();
+
+
+
+  });
 });
